bade5fc7b88b768f81fe28b555f642260b894d2c
ChaPop870 <183779667+ChaPop870@users.noreply.github.com>
2026-04-27 21:48:33 +0200

    Initial Commit

---
+++ src/partial_derivatives.py
@@ -0,0 +1,216 @@
+import numpy as np
+
+import src.total_derivatives
+
+
+class Partial:
+    def __init__(
+            self,
+            function: np.ndarray,
+            x: np.ndarray = None,
+            y: np.ndarray = None
+    ) -> \
+            None:
+        self.function = function
+        self.x = x if x is not None else np.linspace(0, 1, function.shape[1])
+        self.y = y if y is not None else np.linspace(0, 1, function.shape[0])
+
+
+    def x_is_periodic(self) -> bool:
+        """
+        Check if function is periodic in x-direction.
+        Compares first and last columns.
+        """
+        return np.allclose(self.function[:, 0], self.function[:, -1])
+
+
+    def y_is_periodic(self) -> bool:
+        """
+        Check if function is periodic in y-direction.
+        Compares first and last rows.
+        """
+        return np.allclose(self.function[0, :], self.function[-1, :])
+
+
+    def biased_dx(self) -> np.ndarray:
+        """
+        Uses central difference approximation for interior points, forward difference approximation
+         at the first point, and backwards difference approximation at the last point to compute the
+         partial derivative with respect to x-direction.
+        """
+        return np.apply_along_axis(
+            src.total_derivatives.biased_central_difference_approximation,
+            axis=1,
+            arr=self.function,
+            domain=self.x
+        )
+
+
+    def periodic_dx(self) -> np.ndarray:
+        """Uses periodic wrapping at boundaries along with central difference approximation
+         to compute the partial derivative with respect to x-direction."""
+        return np.apply_along_axis(
+            src.total_derivatives.periodic_central_difference_approximation,
+            axis=1,
+            arr=self.function,
+            domain=self.x
+        )
+
+
+    def biased_dy(self) -> np.ndarray:
+        return np.apply_along_axis(
+            src.total_derivatives.biased_central_difference_approximation,
+            axis=0,
+            arr=self.function,
+            domain=self.y
+        )
+
+
+    def periodic_dy(self) -> np.ndarray:
+        return np.apply_along_axis(
+            src.total_derivatives.periodic_central_difference_approximation,
+            axis=0,
+            arr=self.function,
+            domain=self.y
+        )
+
+
+    def dx(self):
+        if self.x_is_periodic():
+            return self.periodic_dx()
+        else:
+            return self.biased_dx()
+
+
+    def dy(self):
+        if self.y_is_periodic():
+            return self.periodic_dy()
+        else:
+            return self.biased_dy()
+
+
+    def biased_dxx(self) -> np.ndarray:
+        return np.apply_along_axis(
+            src.total_derivatives.biased_second_order_derivative_approximation,
+            axis=1,
+            arr=self.function,
+            domain=self.x
+        )
+
+
+    def periodic_dxx(self) -> np.ndarray:
+        return np.apply_along_axis(
+            src.total_derivatives.periodic_second_order_derivative_approximation,
+            axis=1,
+            arr=self.function,
+            domain=self.x
+        )
+
+
+    def biased_dyy(self) -> np.ndarray:
+        return np.apply_along_axis(
+            src.total_derivatives.biased_second_order_derivative_approximation,
+            axis=0,
+            arr=self.function,
+            domain=self.y
+        )
+
+
+    def periodic_dyy(self) -> np.ndarray:
+        return np.apply_along_axis(
+            src.total_derivatives.periodic_second_order_derivative_approximation,
+            axis=0,
+            arr=self.function,
+            domain=self.y
+        )
+
+
+    def dxx(self):
+        """Compute the second order partial derivative with respect to x-direction
+        with the most appropriate scheme."""
+        if self.x_is_periodic():
+            return self.periodic_dxx()
+        else:
+            return self.biased_dxx()
+
+
+    def dyy(self):
+        """Compute the second order partial derivative with respect to y-direction
+        with the most appropriate scheme."""
+        if self.y_is_periodic():
+            return self.periodic_dyy()
+        else:
+            return self.biased_dyy()
+
+
+    def at_x0(self, x0: float) -> np.ndarray:
+        x0 = np.argmin(self.x - x0)
+        return None
+
+
+    def __str__(self) -> str:
+        return (
+            f"Partial(field shape = {self.function.shape},"
+            f"x = {self.x},"
+            f"y = {self.y}),"
+            f"x_periodic={self.x_is_periodic()}, "
+            f"y_periodic={self.y_is_periodic()})"
+        )
+
+
+    def __repr__(self) -> str:
+        return (
+            f"Partial(function=np.array(shape={self.function.shape}), "
+            f"x=np.array(len={len(self.x)}), "
+            f"y=np.array(len={len(self.y)}))"
+        )
+
+
+    def __add__(self, other):
+        if not isinstance(other, Partial):
+            return NotImplemented
+
+        if self.function.shape != other.function.shape:
+            raise ValueError("Function shapes must match.")
+
+        if not np.allclose(self.x, other.x) or not np.allclose(self.y, other.y):
+            raise ValueError("Domains must match.")
+
+        return Partial(
+            self.function + other.function,
+            self.x,
+            other.y
+        )
+
+    def __sub__(self, other):
+        if not isinstance(other, Partial):
+            return NotImplemented
+
+        if self.function.shape != other.function.shape:
+            raise ValueError("Function shapes must match.")
+
+        if not np.allclose(self.x, other.x) or not np.allclose(self.y, other.y):
+            raise ValueError("Domains must match.")
+
+        return Partial(
+            self.function - other.function,
+            self.x,
+            other.y
+        )
+
+    def __mul__(self, other: float):
+        return Partial(self.function * other, self.x, self.y)
+
+# Setup Grid
+x = np.linspace(0, 1, 101)
+y = np.linspace(0, 1, 101)
+X, Y = np.meshgrid(x, y)
+
+# Define stream function
+psi = X * (1 - X) * Y * (1 - Y)
+
+# Define wind components
+u = Partial(psi, x, y).biased_dy()
+v = - Partial(psi, x, y).biased_dx()
+
+print(psi)
---
+++ src/total_derivatives.py
@@ -0,0 +1,292 @@
+# %%
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def biased_forward_difference_approximation(
+        function: np.ndarray,
+        domain: np.ndarray
+) \
+        -> np.ndarray:
+    """
+    Calculates the forward differencing approximation of the first-order
+    derivative in uniform grids using biased backward difference approximation
+    for the first step.
+
+    Parameters
+        domain - an array of the domain of the function.
+        function - an array representing the function to be differentiated.
+
+    Returns - the forward difference approximation of the function.
+    """
+    x = domain
+    y = function
+    h = x[1] - x[0]
+    h_inv = 1 / h
+
+    # Initialize the result.
+    ddx = np.empty_like(y)
+
+    # Forward difference approximation on every other point.
+    ddx[:-1] = h_inv * (y[1:] - y[:-1])
+
+    # Backward difference approximation at last point.
+    ddx[-1] = h_inv * (y[-1] - y[-2])
+
+    return ddx
+
+
+def periodic_forward_difference_approximation(
+        function: np.ndarray,
+        domain: np.ndarray
+) \
+        -> np.ndarray :
+    """
+    Calculates the forward differencing approximation of the first-order
+    derivative in uniform grids using periodic approximation for the end points.
+
+    Parameters
+        domain - an array of the domain of the function.
+        function - an array representing the function to be differentiated.
+
+    Returns - the forward difference approximation of the function.
+    """
+    x = domain
+    y = function
+    h = x[1] - x[0]
+    h_inv = 1 / h
+
+    # Initialize the result.
+    ddx = np.empty_like(y)
+
+    # Forward difference approximation on every other point.
+    ddx[:-1] = h_inv * (y[1:] - y[:-1])
+
+    # Forward difference approximation at last warping the first
+    ddx[-1] = h_inv * (y[0] - y[-2])
+
+    return ddx
+
+
+def biased_backward_difference_approximation(
+        function: np.ndarray,
+        domain: np.ndarray
+) \
+        -> np.ndarray :
+    """
+    Calculates the backward differencing approximation of the first-order
+    derivative in uniform grids using biased forward difference approximation
+    for the last step.
+
+    Parameters
+        domain - an array of the domain of the function.
+        function - an array representing the function to be differentiated.
+
+    Returns - the backward difference approximation of the function.
+    """
+    x = domain
+    y = function
+    h = x[1] - x[0]
+    h_inv = 1 / h
+
+    # Initialize the result
+    ddx = np.empty_like(y)
+
+    # Forward difference approximation at first point.
+    ddx[0] = h_inv * (y[1] - y[0])
+
+    # Backward difference at every point except the first.
+    ddx[1:] = h_inv * (y[1:] - y[:-1])
+
+    return ddx
+
+
+def periodic_backward_difference_approximation(
+        function: np.ndarray,
+        domain: np.ndarray
+) \
+        -> np.ndarray :
+    """
+    Calculates the backward differencing approximation of the first-order
+    derivative in uniform grids using periodic backward differencing with
+    the first step at the last step.
+
+    Parameters
+        domain - an array of the domain of the function.
+        function - an array representing the function to be differentiated.
+
+    Returns - the backward difference approximation of the function.
+    """
+    x = domain
+    y = function
+    h = x[1] - x[0]
+    h_inv = 1 / h
+
+    # Initialize the result
+    ddx = np.empty_like(y)
+
+    # Backward difference approximation at first point.
+    ddx[:-1] = h_inv * (y[1:] - y[:-1])
+
+    # Warping at last point.
+    ddx[-1] = h_inv * (y[-1] - y[-2])
+
+    return ddx
+
+
+def biased_central_difference_approximation(
+        function: np.ndarray,
+        domain: np.ndarray
+) \
+        -> np.ndarray :
+    """
+    Compute the central difference approximation of the first-order derivative
+     in uniform grids using biased forward difference approximation for the first step
+     and biased backward difference approximation for the last step.
+
+    Parameters
+        domain - an array of the domain of the function.
+        function - an array representing the function to be differentiated.
+
+     Returns - the central difference approximation of the function.
+    """
+    x = domain
+    y = function
+    h = x[1] - x[0]
+    h_inv = 1 / h
+
+    # Initialize the result.
+    ddx = np.zeros_like(y)
+
+    # Forward difference approximation at first point.
+    ddx[0] = h_inv * (y[1] - y[0])
+
+    # Central difference at every point except the first and last.
+    ddx[1:-1] = h_inv * 0.5 * (y[2:] - y[:-2])
+
+    # Backward difference approximation at last point.
+    ddx[-1] = h_inv * (y[-1] - y[-2])
+
+    return ddx
+
+
+def periodic_central_difference_approximation(
+        function: np.ndarray,
+        domain: np.ndarray
+) \
+        -> np.ndarray :
+    """
+    Compute the central difference approximation of the first-order derivative
+     in uniform grids using periodic central difference at the boundaries.
+
+    Parameters
+        domain - an array of the domain of the function.
+        function - an array representing the function to be differentiated.
+
+     Returns - the central difference approximation of the function.
+    """
+    x = domain
+    y = function
+    h = x[1] - x[0]
+    h_inv = 1 / h
+
+    # Initialize the result.
+    ddx = np.zeros_like(y)
+
+    # Central difference approximation at first point with last point.
+    ddx[0] = h_inv * 0.5 * (y[1] - y[-2])
+
+    # Central difference at interior points.
+    ddx[1:-1] = h_inv * 0.5 * (y[2:] - y[:-2])
+
+    # Central difference approximation at last point with first point.
+    ddx[-1] = h_inv * 0.5 * (y[0] - y[-3])
+
+    return ddx
+
+
+def biased_second_order_derivative_approximation(
+        function: np.ndarray,
+        domain: np.ndarray
+) \
+        -> np.ndarray :
+    """
+    Compute the second order derivative approximation of the function
+    in uniform grids using biased forward difference approximation for the
+    first step and biased backward difference approximation for the last step.
+
+    Parameters
+        domain - an array of the domain of the function.
+        function - an array representing the function to be differentiated.
+
+    Returns - the second order derivative approximation of the function.
+    """
+    x = domain
+    y = function
+    h = x[1] - x[0]
+    h_inv = 1 / h
+
+    # Initialize the result.
+    ddx = np.zeros_like(y)
+
+    # Forward difference approximation at first point.
+    ddx[0] = h_inv * h_inv * (y[0] - 2 * y[1] + y[2])
+
+    # Interior central difference approximation in interior.
+    ddx[1:-1] = h_inv * h_inv * (y[:-2] - 2 * y[1:-1] + y[2:])
+
+    # Backward difference approximation at last point.
+    ddx[-1] = h_inv * h_inv * (y[-3] - 2 * y[-2] + y[-1])
+
+    return ddx
+
+
+def periodic_second_order_derivative_approximation(
+        function: np.ndarray,
+        domain: np.ndarray
+) \
+        -> np.ndarray :
+    """
+    Compute the second order derivative approximation of the function
+    in uniform grids using biased forward difference approximation for the
+    first step and biased backward difference approximation for the last step.
+
+    Parameters
+        domain - an array of the domain of the function.
+        function - an array representing the function to be differentiated.
+
+    Returns - the second order derivative approximation of the function.
+    """
+    x = domain
+    y = function
+    h = x[1] - x[0]
+    h_inv = 1 / h
+
+    # Initialize the result.
+    ddx = np.zeros_like(y)
+
+    # Central difference approximation at first point with last point.
+    ddx[0] = h_inv * h_inv * (y[-2] - 2 * y[0] + y[1])
+
+    # Interior central difference approximation in interior.
+    ddx[1:-1] = h_inv * h_inv * (y[:-2] - 2 * y[1:-1] + y[2:])
+
+    # Backward difference approximation at last point.
+    ddx[-1] = h_inv * h_inv * (y[-2] - 2 * y[-1] + y[1])
+
+    return ddx
+
+
+def main():
+    x = np.linspace(-1, 1, 1_000)
+    f1 = x
+    f2 = np.sin(np.pi * x)
+
+    fig, ax = plt.subplots()
+    ax.plot(x, periodic_second_order_derivative_approximation(f2, x))
+    ax.set_ylim(-10, 10)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
