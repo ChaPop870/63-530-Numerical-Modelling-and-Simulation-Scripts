d108c23e6d649b0336802e39884282ed468613b8
ChaPop870 <183779667+ChaPop870@users.noreply.github.com>
2026-06-13 14:45:20 +0200

    Defined transformation matrix for the second derivative with periodic BC.

---
+++ src/diffusion_advection.py
@@ -0,0 +1,18 @@
+
+kappa = 1.0
+c = 1.0
+
+
+def transformation_dxx(n: int) -> np.ndarray:
+    """Defines the n x n transformation matrix necessary to solve the
+    second order partial derivative wrt x using the periodic boundary
+    condition."""
+    H = np.zeros((n, n))
+    H += np.diag(-2 * np.ones(n))
+    H += np.diag(np.ones(n - 1), k=1)
+    H += np.diag(np.ones(n - 1), k=-1)
+    H[0, -1] = 1
+    return H
+
+
+print(transformation_dxx(5))
