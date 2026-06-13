57ed70681f4940d4fbac841e25af920856dfde12
ChaPop870 <183779667+ChaPop870@users.noreply.github.com>
2026-06-13 14:53:28 +0200

    Defined transformation matrix to solve the first partial derivative.

--- src/diffusion_advection.py
+++ src/diffusion_advection.py
@@ -3,6 +3,17 @@ kappa = 1.0
 c = 1.0
 
 
+def transformation_dx(n):
+    """Defines the n x n transformation matrix necessary to solve the
+    first-order partial derivative wrt x using the periodic boundary
+    condition."""
+    H = np.zeros((n, n))
+    H += np.diag(np.ones(n - 1), k=1)
+    H += np.diag(-1 * np.ones(n - 1), k=-1)
+    H[0, -1] = -1
+    return H
+
+
 def transformation_dxx(n: int) -> np.ndarray:
     """Defines the n x n transformation matrix necessary to solve the
     second order partial derivative wrt x using the periodic boundary
